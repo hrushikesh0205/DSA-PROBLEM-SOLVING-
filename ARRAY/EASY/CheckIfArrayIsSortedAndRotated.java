f3c5d726d2a1a5cb5e93574231375d5909fdd873
Hrushikesh Bhoir <rushikesh72@gmail.com>
2026-01-17 18:58:28 +0530

    Fixed: Syntax error in CheckIfArrayIsSortedAndRotated - Accepted solution

--- ARRAY/EASY/CheckIfArrayIsSortedAndRotated.java
+++ ARRAY/EASY/CheckIfArrayIsSortedAndRotated.java
@@ -12,18 +12,17 @@ CheckIfArrayIsSortedAndRotated Array Is Sorted and Rotated
 //  Time Complexity: O(n) - Single pass through array
 //  Space Complexity: O(1) - Only using counter variable
  
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
 
-public boolean check(int[] nums) {
-    int count = 0;
-    int n = nums.length;
-    
-    for(int i = 0; i < n; i++) {
-        if(nums[i] > nums[(i + 1) % n]) {
-            count++;
-        }    
-        if(count > 1) {
-            return false;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+            if (count > 1) return false;
         }
-    }
         return true;
+    }
 }
