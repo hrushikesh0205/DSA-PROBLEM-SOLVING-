9e93f0bc32a2cca9336ca0e69c51720ba42a608a
Hrushikesh Bhoir <rushikesh72@gmail.com>
2026-01-17 18:45:35 +0530

    Added: Two Sum problem solution

--- ARRAY/EASY/CheckIfArrayIsSortedAndRotated.java
+++ ARRAY/EASY/CheckIfArrayIsSortedAndRotated.java
@@ -1,22 +1,7 @@
 
 CheckIfArrayIsSortedAndRotated Array Is Sorted and Rotated
 //  LeetCode: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
-//  Difficulty: Easy
- 
-//  Problem Statement:
-//  Given an array nums, return true if the array was originally sorted in 
-//  non-decreasing order, then rotated some number of positions (including zero).
-//   Otherwise, return false.
-  
-//  Example 1:
-//  Input: nums = [3,4,5,1,2]
-//  Output: true
-//   Explanation: [1,2,3,4,5] is the original sorted array, rotated 3 times.
-//  * 
-// Example 2:
-//  Input: nums = [2,1,3,4]
-//   Output: false
-  
+
 //  * Approach:
 //  Count the number of "break points" where nums[i] > nums[i+1]
 //  A sorted array has 0 break points
